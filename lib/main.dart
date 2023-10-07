8d398d24ad2378dedb3916a805241492cad98215
Nasibali144 <abdiyevnasibali@gmail.com>
2023-10-07 18:24:35 +0500

    commit 2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:learn_web_socket/model/order_book_model.dart';
 import 'package:learn_web_socket/service/socket_service.dart';
 
 void main() {
   SocketService.init();
-  SocketService.send();
   runApp(const MyApp());
 }
 
@@ -20,49 +20,67 @@ class MyApp extends StatelessWidget {
 }
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  OrderBook? orderBook;
+
+  @override
+  void initState() {
+    super.initState();
+    SocketService.send();
+    SocketService.read().listen((event) {
+      orderBook = event;
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    if(orderBook == null) {
+      return const Scaffold(
+        body: Center(
+          child: Text("No Data"),
+        ),
+      );
+    }
+
     return Scaffold(
-      body: StreamBuilder(
-        stream: SocketService.read(),
-        builder: (context, snapshot) {
-          if(!snapshot.hasData) {
-            return const Center(
-              child: Text("No Data"),
-            );
-          }
-
-          return Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Text("Symbol", style: Theme.of(context).textTheme.titleMedium,),
-                    const SizedBox(width: 20,),
-                    Expanded(child: Text(snapshot.data?.symbol ?? "No data", style: Theme.of(context).textTheme.bodyMedium,)),
-                  ],
-                ),
-
-                Row(
-                  children: [
-                    Text("Data", style: Theme.of(context).textTheme.titleMedium,),
-                    const SizedBox(width: 20,),
-                    Expanded(child: Text(snapshot.data?.data.toString() ?? "No data", style: Theme.of(context).textTheme.bodyMedium,)),
-                  ],
-                ),
+                Text("Symbol", style: Theme.of(context).textTheme.titleMedium,),
+                const SizedBox(width: 20,),
+                Expanded(child: Text(orderBook?.symbol ?? "No data", style: Theme.of(context).textTheme.bodyMedium,)),
               ],
             ),
-          );
-        },
+
+            Row(
+              children: [
+                Text("Data", style: Theme.of(context).textTheme.titleMedium,),
+                const SizedBox(width: 20,),
+                Expanded(child: Text(orderBook?.data.toString() ?? "No data", style: Theme.of(context).textTheme.bodyMedium,)),
+              ],
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SocketService.close();
   }
 }
